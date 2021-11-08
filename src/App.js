84997338af6701a5ec0deca8a9bc5e3377e11369
samamoazemi <moazemisama@gmail.com>
2021-11-08 05:51:28 -0800

    map on Products state

--- src/App.js
+++ src/App.js
@@ -1,32 +1,23 @@
 import react from "react";
 import Product from "./components/product/Product";
 
-class App extends react.Component{
+class App extends react.Component {
+  state = {
+    products: [
+       { title:"React.js", price:"99 $" },
+       { title:"Node.js", price:"89 $" },
+       { title:"JavaScript", price:"79 $" },
+    ],
+  };
   render(){
     return(
       <div className="container" id="title">
          <h1>shopping App</h1>
-         <Product name="React.js" price="99 $" />
-         <Product name="Node.js" price="89 $" />
-         <Product name="JavaScript" price="79$">
-             <p>discout is 15%</p>
-         </Product>
+         {this.state.products.map((product) => {
+           return <Product name={product.title} price={product.price} />;
+         })}
        </div>
-    )
+    );
   }
 }
-
-// const App = () => {
-//     return (
-//       <div className="container" id="title">
-//         <h1>shopping App</h1>
-//         <Product name="React.js" price="99 $" />
-//         <Product name="Node.js" price="89 $" />
-//         <Product name="JavaScript" price="79$">
-//             <p>discout is 15%</p>
-//         </Product>
-//       </div>
-//     );
-// };
-
 export default App;
