5e50c833f9ec71d3635c04ae2a0c491a5927e45c
samamoazemi <moazemisama@gmail.com>
2021-11-08 07:44:39 -0800

    onClick event in React

--- src/App.js
+++ src/App.js
@@ -9,6 +9,17 @@ class App extends react.Component {
        { title:"JavaScript", price:"79 $" },
     ],
   };
+
+clickhandler = () => {
+  this.setState({
+    products: [
+       { title:"React.js", price:"89 $" },
+       { title:"Node.js", price:"79 $" },
+       { title:"JavaScript", price:"69 $" },
+    ],
+  })
+}
+
   render(){
     return(
       <div className="container" id="title">
@@ -16,6 +27,7 @@ class App extends react.Component {
          {this.state.products.map((product) => {
            return <Product name={product.title} price={product.price} />;
          })}
+         <button onClick={this.clickhandler}>change price</button>
        </div>
     );
   }
