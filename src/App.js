91a3fa31229998a7f7e036f98e15068e8c302b07
samamoazemi <moazemisama@gmail.com>
2021-11-08 04:31:11 -0800

    convert App to Class

--- src/App.js
+++ src/App.js
@@ -1,16 +1,32 @@
+import react from "react";
 import Product from "./components/product/Product";
 
-const App = () => {
-    return (
+class App extends react.Component{
+  render(){
+    return(
       <div className="container" id="title">
-        <h1>shopping App</h1>
-        <Product name="React.js" price="99 $" />
-        <Product name="Node.js" price="89 $" />
-        <Product name="JavaScript" price="79$">
-            <p>discout is 15%</p>
-        </Product>
-      </div>
-    );
-};
+         <h1>shopping App</h1>
+         <Product name="React.js" price="99 $" />
+         <Product name="Node.js" price="89 $" />
+         <Product name="JavaScript" price="79$">
+             <p>discout is 15%</p>
+         </Product>
+       </div>
+    )
+  }
+}
+
+// const App = () => {
+//     return (
+//       <div className="container" id="title">
+//         <h1>shopping App</h1>
+//         <Product name="React.js" price="99 $" />
+//         <Product name="Node.js" price="89 $" />
+//         <Product name="JavaScript" price="79$">
+//             <p>discout is 15%</p>
+//         </Product>
+//       </div>
+//     );
+// };
 
 export default App;
