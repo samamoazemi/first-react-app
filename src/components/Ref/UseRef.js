44bfa402ceb33218623086b9ae6bcebc22b86682
samamoazemi <moazemisama@gmail.com>
2021-11-23 01:17:04 -0800

    store previous state with useRef

--- src/components/Ref/UseRef.js
+++ src/components/Ref/UseRef.js
@@ -1,30 +1,43 @@
-import { useRef, useState } from "react";
+import { useEffect, useRef, useState } from "react";
 
 const UseRefExample = () => {
 
     const[inputValue, setInputValue] = useState("")
-    const inputRef = useRef()
+    const[count, setCount] = useState(0)
 
-    const resetHandler = () => {
-        setInputValue("")
-        inputRef.current.focus()
-
-    }
+    const previousValue = useRef()
+    const previousCount = useRef()
 
     const changeHandler = (e) => {
         setInputValue(e.target.value)
-
     }
+    // console.log("re-render", inputValue)
+    // console.log("prev-value", previousValue.current)
+
+    useEffect(() => {
+        previousValue.current = inputValue; // does not re-render component
+        console.log("useEffect", previousValue.current)
+    },[inputValue])
+    
+    useEffect(() => {
+        previousCount.current = count; // does not re-render component
+    },[count])
 
     return(
         <div>
             <input 
             type="text" 
-            ref={inputRef} 
             onChange={changeHandler} 
             value={inputValue} 
             />
-            <button onClick={resetHandler}>Reset</button>
+            <p> my name is {inputValue} and it use to be {previousValue.current} </p>
+            <div>
+            <button  onClick={() => setCount(Math.ceil(Math.random()*100))}>
+                generate random
+            </button>
+                <div>count is : {count}</div>
+                <div>previous count is : {previousCount.current} </div>
+            </div>
         </div>
     )
 }
